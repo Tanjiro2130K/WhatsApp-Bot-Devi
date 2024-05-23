c1f62c247996ff84c37265018ccc47da459d5f34
Tanjiro2130K <127303425+Tanjiro2130K@users.noreply.github.com>
2024-05-23 11:18:24 +0530

    Update Sticker.js

--- src/commands/utils/Sticker.js
+++ src/commands/utils/Sticker.js
@@ -27,8 +27,8 @@ export default class Command extends BaseCommand {
         const sticker = new Sticker(
             typeof media === 'string' ? media : await this.client.util.downloadMediaMessage(media),
             {
-                pack: pack ?? `${this.client.config.name} Bot`,
-                author: title ?? 'By Debanjan',
+                pack: pack ?? `TIKU✨🦋`,
+                author: title ?? '+91 7086726371',
                 type: StickerTypes.FULL,
                 quality: 70
             }
